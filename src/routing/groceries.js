2fe1dfa5042a086daa6e7ce04f9aa7bc7bd99dc0
kazet <kazet15@gmail.com>
2015-11-09 20:16:41 +0100

    add grocery list

--- src/routing/groceries.js
+++ src/routing/groceries.js
@@ -2,11 +2,22 @@
 
 import { Router } from 'express';
 import auth from '../auth';
+import redis from '../redis';
 
 let router = new Router();
 
 router.get('/groceries', auth, (req, res) => {
-  res.json([]).end();
+  redis.keys(`user-${req.user.name}-groceries-*`, (error, keys) => {
+    redis.mget(keys, (error, values) => {
+      keys = keys.map(key => key.split('-').slice(-1)[0]);
+
+      let groceries = {};
+
+      keys.forEach((key, index) => groceries[key] = values[index]);
+
+      res.json(groceries).end();
+    });
+  });
 });
 
 export default router;
--- src/routing/login.js
+++ src/routing/login.js
@@ -11,26 +11,26 @@ const COOKIE = config.get('jwt.cookie');
 let router = new Router();
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+  let { name, password } = req.body;
 
-  if (!username || !password) {
-    res.status(400).end();
+  if (!name || !password) {
+    return res.status(400).end();
   }
 
-  redis.get(`user-${username}`, (error, response) => {
+  redis.get(`user-${name}`, (error, response) => {
     if (!!error) {
-      res.status(500).end();
+      return res.status(500).end();
     }
 
     if (!response || password !== response) {
-      res.status(403).end();
+      return res.status(403).end();
     }
 
     let token = jwt.sign({
-      username: username
+      name: name
     }, SECRET);
 
-    res
+    return res
       .cookie(COOKIE, token)
       .status(200)
       .end();
